2022b3efcb0842af6bebcd3bb8dd1f10c3f6517f
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-13 00:23:12 +0200

    Try manually cloning `cubical`

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo "Main file: $1"
 echo "Source directory: $2"
 agda --version
 cd $2
+git clone https://github.com/agda/cubical.git
+mv cubical/Cubical .
 agda --safe $1 || exit 1
 time=$(date)
 echo "::set-output name=time::$time"
