dce886c952f2a7c3ad6f4ce4db6e40a5f209fe8f
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-13 11:36:16 +0200

    Try to get setup.sh to work

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-git clone
-wget https://github.com/agda/agda-stdlib/archive/v1.3.tar.gz -O /root/agda-stdlib.tar.gz
-cd /root && tar -xvf agda-stdlib.tar.gz && cd /github/workspace/example
-echo 'standard-library' > /root/.agda/defaults
-echo '/root/agda-stdlib/standard-library.agda-lib'
+pwd
+# wget https://github.com/agda/agda-stdlib/archive/v1.3.tar.gz -O /root/agda-stdlib.tar.gz
+# cd /root && tar -xvf agda-stdlib.tar.gz && cd /github/workspace/example
+# echo 'standard-library' > /root/.agda/defaults
+# echo '/root/agda-stdlib/standard-library.agda-lib'
