badc01e6f1bcdb0e7a60729dae48e5d0af081781
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-13 14:01:30 +0200

    Continue attempt to include the standard library

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ agda --version
 cd $2
 git clone https://github.com/agda/cubical.git
 mv cubical/Cubical .
-agda --safe $1 || exit 1
-agda --html $1 && mv html ..
+wget https://github.com/agda/agda-stdlib/archive/v1.3.tar.gz
+tar -xvf v.1.3.tar.gz
+agda --library-file=v1.3/standard-library.agda-lib --safe $1 || exit 1
+agda --library-file=v1.3/standard-library.agda-lib --html $1 && mv html ..
