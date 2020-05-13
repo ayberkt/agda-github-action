e689b6f44354ed34ef62ce10098b76c39a27baff
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-13 12:09:08 +0200

    Use the correct file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Source directory: $2"
 agda --version
 cd $2
 wget https://github.com/agda/agda-stdlib/archive/v1.3.tar.gz
-tar -xvf agda-stdlib.tar.gz v1.3.tar.gz
+tar -xvf v1.3.tar.gz
 mkdir /root/.agda
 mv v1.3 /root/agda-stdlib
 git clone https://github.com/agda/cubical.git
