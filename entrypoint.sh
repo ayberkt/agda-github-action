80fa122556173d384b50c77ac1bdc1f1dcbdfc24
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-13 14:13:57 +0200

    Fix name of stdlib directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ git clone https://github.com/agda/cubical.git
 mv cubical/Cubical .
 wget https://github.com/agda/agda-stdlib/archive/v1.3.tar.gz
 tar -xvf v1.3.tar.gz
-agda --library-file=v1.3/standard-library.agda-lib --safe $1 || exit 1
-agda --library-file=v1.3/standard-library.agda-lib --html $1 && mv html ..
+agda --library-file=agda-stdlib-1.3/standard-library.agda-lib --safe $1 || exit 1
+agda --library-file=agda-stdlib-1.3/standard-library.agda-lib --html $1 && mv html ..
